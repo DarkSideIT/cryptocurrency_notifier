e59234d89462993ad90cbfba821661627caa40e4
DarkSideIT <107682338+DarkSideIT@users.noreply.github.com>
2023-07-03 12:13:20 +0800

    add tg bot for notify

---
+++ currency/handlers/telegram.py
@@ -0,0 +1,8 @@
+import telebot
+bot = telebot.TeleBot("6386555418:AAGxHBU4Pq3B1rvFBJ0IOftlwuKrCk9hh2w")
+
+def send_notify(message):
+    bot = telebot.TeleBot("6386555418:AAGxHBU4Pq3B1rvFBJ0IOftlwuKrCk9hh2w")
+    bot.send_message(756664112, message)
+
+    
